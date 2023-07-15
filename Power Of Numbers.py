c57fbe6ac750e27927868d902460f0bcdc0f4948
Divya P S <85628080+Div-ya18@users.noreply.github.com>
2023-07-15 10:14:35 +0530

    Create Power Of Numbers.py

---
+++ Power Of Numbers.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    def power(self,N,R):
+        #Your code here
+        n=1000000007
+        return pow(N,R,n)
